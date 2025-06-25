cccd4220f33121ee5d157baeb38bfd3b1dfd2f04
Simran Yadav <simranyadav464@gmail.com>
2025-06-26 02:19:40 +0530

    Updated the shell script

--- extract_nav.sh
+++ extract_nav.sh
@@ -1,13 +1,9 @@
-#!/bin/bash
-
-# File to save raw data
 curl -s "https://www.amfiindia.com/spages/NAVAll.txt" -o nav.txt
 
-# Output TSV file
+
 output_file="scheme_nav.tsv"
 echo -e "Scheme Name\tAsset Value" > "$output_file"
 
-# Extract and format
 awk -F ';' '
 BEGIN { OFS="\t" }
 /^[0-9]+;/ {
@@ -18,4 +14,4 @@ BEGIN { OFS="\t" }
     }
 }' nav.txt >> "$output_file"
 
-echo "✅ Extracted Scheme Name and NAV into $output_file"
+echo "Extracted Scheme Name and NAV into $output_file"
